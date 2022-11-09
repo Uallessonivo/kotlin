bcf1c37f1e23751e2b94feba092836a89a291e58
Uallessonivo <uallessons@gmail.com>
2022-11-09 15:03:31 -0300

    updating

---
+++ src/main/course/NumberTypes.kt
@@ -0,0 +1,16 @@
+fun numberTypes() {
+    // Byte, Short, Int, Long, Float, Double
+
+    // Explicit
+    val items: Int = 5
+    // Implicit
+    val itemV2 = 10
+
+    val nOfPeople: Long = 7500000000
+    val price: Double = 29.99
+
+    println(items)
+    println(itemV2)
+    println(nOfPeople)
+    println(price)
+}
