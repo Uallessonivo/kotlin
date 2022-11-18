fefb509c9c00ad31499527f4cd3ff43ce1bf2a57
Uallessonivo <uallessons@gmail.com>
2022-11-18 10:51:53 -0300

    updating

---
+++ src/main/kotlin/Overloading.kt
@@ -0,0 +1,22 @@
+fun main() {
+    // Two functions can have the same name if they have the different numbers or types of parameters
+    println(multiply(5))
+    println(multiply(5, 5))
+
+    println(sayHello("Uallesson"))
+    println(sayHello(listOf("Uallesson", "Lua", "Neuza")))
+}
+
+fun multiply(number: Int) = number * 2
+fun multiply(number: Int, multiplier: Int) = number * multiplier
+
+
+fun sayHello(person: String) {
+    println("Hello $person!")
+}
+
+fun sayHello(people: Collection<String>) {
+    for (person in people) {
+        println("Hello $person!")
+    }
+}
