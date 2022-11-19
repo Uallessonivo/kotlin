dbef365218bd55b10b2018918ad1e4c061d96fc3
Uallesson Nunes Ivo <uallessons@gmail.com>
2022-11-19 06:41:01 -0300

    updating

---
+++ src/main/kotlin/Constructors.kt
@@ -0,0 +1,33 @@
+fun main() {
+    val table1 = Table()
+    val table2 = Table(1)
+    val table3 = Table(6,175)
+
+    table1.printInfo()
+    table2.printInfo()
+    table3.printInfo()
+}
+
+class Table {
+    constructor() {
+        legs = 3
+        height = 120
+    }
+
+    constructor(newLegs: Int) {
+        legs = newLegs
+        height = 120
+    }
+
+    constructor(newLegs: Int, newHeight: Int) {
+        legs = newLegs
+        height = newHeight
+    }
+
+    var legs = 4
+    var height = 150
+
+    fun printInfo() {
+        println("This table has $legs legs and a height of $height")
+    }
+}
